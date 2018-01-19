5c7af6790f7e9639f82823da0e78719a185f19f2
Aditya Agarwal <adityaa803@gmail.com>
2018-01-19 15:31:50 +0530

    fixed rel path bug

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
 const program = require('commander');
 const open = require("open");
 const fs = require("fs");
+const path = require('path');
 
 program
     .version('1.0.0')
     .option('-f, --file', 'generate code image for which file')
     .action(file => {
-        const snippet = fs.readFileSync(file);
-        const url = `https://carbon.now.sh/?bg=rgba(66,66,72,1)&t=seti&l=auto&ds=true&wc=true&wa=true&pv=48px&ph=32px&ln=false&code=${encodeURI(snippet)}`;
+        file = path.join(process.cwd(), file)
+        const snippet = fs.readFileSync(file).toString();
+        if(snippet.length >= 1500) {
+          console.log('File too big !!');
+          return;
+        }
+        const url = `https://carbon.now.sh/?bg=rgba(66,66,72,1)&t=seti&l=auto&ds=true&wc=true&wa=true&pv=48px&ph=32px&ln=false&code=${encodeURIComponent(snippet)}`;
         open(url, "chrome");
     })
     .parse(process.argv);
